be5b5b4f10ad600033b9127c0f201f20621c0155
Derek Willis <dwillis@gmail.com>
2010-06-04 04:42:53 +0800

    added getByZip responses

--- lib/vote_smart/official.rb
+++ lib/vote_smart/official.rb
@@ -126,6 +126,10 @@ module VoteSmart
     def self.get_by_district district_id
       request("Officials.getByDistrict", "districtId" => district_id)
     end
-    
+
+    # Returns incumbents in the provided zip code, with optional zip+4 and stage
+    def self.get_by_zip zip5, election_year=nil, zip4=nil, stage_id=nil
+      request("Officials.getByZip", "zip5" => zip5, "electionYear" => election_year, "zip4" => zip4, "stageId" => stage_id)
+    end
   end
 end
