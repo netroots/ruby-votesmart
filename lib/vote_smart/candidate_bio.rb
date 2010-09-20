d6c24064f882f9086d31da5514ed572d6f7bc1d4
Ben Woosley <ben.woosley@gmail.com>
2010-09-20 03:16:49 -0400

    Include CandiateBio.get_bio in the parallelizable requests

--- lib/vote_smart/candidate_bio.rb
+++ lib/vote_smart/candidate_bio.rb
@@ -3,8 +3,8 @@ module VoteSmart
   class CandidateBio < Common
     
     # Returns basic bio details on a candidate
-    def self.get_bio can_id
-      request("CandidateBio.getBio", "candidateId" => can_id)
+    def self.get_bio can_id, &block
+      request("CandidateBio.getBio", {"candidateId" => can_id}, &block)
     end
     
     # 
